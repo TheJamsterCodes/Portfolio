75b31f14418a06f5e894d85bfcbdcf5d566797c0
jalanis00 <FJam07@gmail.com>
2018-11-18 16:59:01 -0600

    Added edit and update functionality.

--- app/controllers/my_portfolios_controller.rb
+++ app/controllers/my_portfolios_controller.rb
@@ -1,27 +1,43 @@
 class MyPortfoliosController < ApplicationController
 
-    def index
-        @portfolio_items = MyPortfolio.all
-    end
+  def index
+    @portfolio_items = MyPortfolio.all
+  end
 
-    def show
-        
-    end
+  def show
+      
+  end
 
-    def new
-        @portfolio_item = MyPortfolio.new
-    end
+  def new
+    @portfolio_item = MyPortfolio.new
+  end
 
-    def create
-        @portfolio_item = MyPortfolio.new(params.require(:my_portfolio).permit(:title, :subtitle, :body))
+  def create
+    @portfolio_item = MyPortfolio.new(params.require(:my_portfolio).permit(:title, :subtitle, :body))
 
-        respond_to do |format|
-            if @portfolio_item.save
-                format.html { redirect_to my_portfolios_path, notice: 'Your portfolio item is now live.' }
-            else
-                format.html { render :new }
-            end
-        end
+    respond_to do |format|
+      if @portfolio_item.save
+          format.html { redirect_to my_portfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
     end
+  end
+
+  def update
+    @portfolio_item = MyPortfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:my_portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to my_portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def edit
+    @portfolio_item = MyPortfolio.find(params[:id])
+  end
 
 end
