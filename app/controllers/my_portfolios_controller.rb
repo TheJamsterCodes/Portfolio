98e32ab0f671d0db0eafadcf84665553d9acadd0
jalanis00 <FJam07@gmail.com>
2018-11-18 14:59:50 -0600

    Integrated new actions for portoflios.

--- app/controllers/my_portfolios_controller.rb
+++ app/controllers/my_portfolios_controller.rb
@@ -4,4 +4,24 @@ class MyPortfoliosController < ApplicationController
         @portfolio_items = MyPortfolio.all
     end
 
+    def show
+        
+    end
+
+    def new
+        @portfolio_item = MyPortfolio.new
+    end
+
+    def create
+        @portfolio_item = MyPortfolio.new(params.require(:my_portfolio).permit(:title, :subtitle, :body))
+
+        respond_to do |format|
+            if @portfolio_item.save
+                format.html { redirect_to my_portfolios_path, notice: 'Your portfolio item is now live.' }
+            else
+                format.html { render :new }
+            end
+        end
+    end
+
 end
