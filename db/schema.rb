396a04091f2440e6a33fada20084f797ac53da0e
jalanis00 <FJam07@gmail.com>
2018-11-20 22:23:39 -0600

    Fixed add_post_status_to_blogs migration issue.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_023616) do
+ActiveRecord::Schema.define(version: 2018_11_21_041754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,8 @@ ActiveRecord::Schema.define(version: 2018_11_21_023616) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
-    t.integer "status"
+    t.integer "status", default: 0
+    t.integer "post_status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
