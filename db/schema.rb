2e5ae7b517238525998e2cd24e264735a96ee09d
jalanis00 <FJam07@gmail.com>
2018-11-21 22:38:02 -0600

    Implemented Placeholder concern for managing image data.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_230149) do
+ActiveRecord::Schema.define(version: 2018_11_22_035625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_230149) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
