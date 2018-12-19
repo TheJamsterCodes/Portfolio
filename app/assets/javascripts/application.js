53af20bbf1e8c86a4cbf477891f9007edde3ef96
James Alanis <fjam07@gmail.com>
2018-12-19 00:12:25 -0600

    Added webpacker to fix collapse issue with hamburger icon in the menu. Implemented initial design on the portfolio index page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
