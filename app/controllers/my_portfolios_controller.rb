019c0b3223f518ad39d2eedf6e25b8f6f05ac7c6
James Alanis <fjam07@gmail.com>
2018-12-20 00:56:56 -0600

    Added authorization for site_admin for drag and drop feature.

--- app/controllers/my_portfolios_controller.rb
+++ app/controllers/my_portfolios_controller.rb
@@ -1,7 +1,7 @@
 class MyPortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout "my_portfolio"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
 
   def index
     @portfolio_items = MyPortfolio.by_position
@@ -11,7 +11,7 @@ class MyPortfoliosController < ApplicationController
     params[:order].each do |key, value|
       Portfolio.find(value[:id]).update(position: value[:position])
     end
-    
+
     render nothing: true
   end
 
