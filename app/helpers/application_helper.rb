75b31f77189b85ae5529782bccd81bf7ada2d038
James Alanis <fjam07@gmail.com>
2018-12-31 01:42:44 -0600

    Refactored alerts into helper method.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,4 +56,15 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "James Alanis' Portfolio", sticky: false)
+  end
+
 end
